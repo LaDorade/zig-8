a750d96408b9aa83827f278e6eb1ec44c31fafe5
Mathis <mathisjung02@gmail.com>
2026-01-05 23:36:50 +0100

    process cli args

--- src/main.zig
+++ src/main.zig
@@ -37,6 +37,62 @@ fn show_display(display: *Display) !void {
     try stdout.flush();
 }
 
+var PROGRAM_NAME: []const u8 = undefined;
+const USAGE_FMT =
+    \\
+    \\Usage: {s} ROM_PATH [--cycle=freq]
+    \\
+    \\Arguments:
+    \\  ROM_PATH    relative or absolute path to the ROM you want to play
+    \\
+    \\Options:
+    \\  --cycle=freq number of instruction processed each second by the cpu
+    \\      Defaults to 700
+    \\
+;
+
+fn display_usage() noreturn {
+    std.log.info(USAGE_FMT, .{PROGRAM_NAME});
+    std.process.exit(0);
+}
+
+const CLIArgs = struct {
+    rom_path: []const u8 = undefined,
+    cycle: u32 = 700,
+};
+
+fn parseArgs(args: []const [:0]const u8) CLIArgs {
+    var cliArgs = CLIArgs{};
+    PROGRAM_NAME = std.fs.path.basename(args[0]);
+
+    if (args.len < 2) {
+        std.log.err("Missing ROM_PATH arg", .{});
+        display_usage();
+    }
+
+    cliArgs.rom_path = args[1];
+    var index: usize = 2;
+    while (index < args.len) {
+        if (std.mem.startsWith(u8, args[index], "--cycle")) {
+            var cycleIter = std.mem.splitAny(u8, args[index], "=");
+
+            _ = cycleIter.next() orelse unreachable;
+            const cycleVal = cycleIter.next() orelse {
+                std.log.err("Not enough arguments for cycle frequency", .{});
+                display_usage();
+            };
+
+            const cycleValInt = std.fmt.parseInt(u32, cycleVal, 10) catch {
+                std.log.err("Invalid number \"{s}\" for cycle frequency", .{cycleVal});
+                display_usage();
+            };
+            cliArgs.cycle = cycleValInt;
+        }
+        index += index;
+    }
+    return cliArgs;
+}
+
 pub fn main() !void {
     var debugAlloc = std.heap.DebugAllocator(.{}){};
     var ar = std.heap.ArenaAllocator.init(debugAlloc.allocator());
@@ -47,22 +103,18 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(areno);
     defer std.process.argsFree(areno, args);
 
-    if (args.len != 2) {
-        std.log.err("Must provide a ROM path", .{});
-        return;
-    }
+    const parsed_args = parseArgs(args);
 
-    const file_path = args[1];
     const rom_data = try std.fs.cwd().readFileAlloc(
         areno,
-        file_path,
+        parsed_args.rom_path,
         4096,
     );
 
     var cpu = zig8.CPU{};
     try cpu.load_RAM(rom_data);
 
-    const cycle_freq_hz = 700;
+    const cycle_freq_hz = parsed_args.cycle;
     const display_freq_hz = 60;
     const display_period_ms = std.time.ms_per_s / display_freq_hz;
 
